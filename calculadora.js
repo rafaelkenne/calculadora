b2a3566a8188c56bac1b8c473d47a6babb835fe1
rafaelkenne <rakenvi@gmail.com>
2023-09-22 15:27:45 -0300

    add arquivos

---
+++ calculadora.js
@@ -0,0 +1,50 @@
+let $ = document.querySelector.bind(document);
+let painel1 = $('#painel1');
+let painel2 = $('#painel2');
+let contador = 0;
+let array = [];
+
+
+function Inserir(valor) {
+  array.push(valor);
+  painel1.innerText += valor;
+  console.log(array);
+}
+
+function Limpar() {
+  painel1.innerText = '';
+  painel2.innerText = '';
+  contador = 0;
+  array = [];
+}
+
+function Calcular() {
+  let calculo = painel1.innerText;
+  painel2.innerText = eval(calculo);
+}
+
+function BotaoIgual() {
+  let calculo = painel1.innerText;
+  painel1.innerText = eval(calculo);
+  painel2.innerText = '';
+}
+
+function sinalParenteses() {
+  let numeroOuSinal = array.slice(-1);
+  if (contador == 0 && Number(numeroOuSinal)) {
+    painel1.innerText += "*(";
+    contador = 1;
+    return;
+  }
+  if (contador == 0 && !Number(numeroOuSinal)) {
+    painel1.innerText += "(";
+    contador = 1;
+    return;
+  }
+  if (contador == 1) {
+    painel1.innerText += ")";
+    contador = 0;
+  }
+}
+
+
